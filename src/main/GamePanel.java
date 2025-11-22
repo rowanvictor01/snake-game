1993974a8b4ad9bda9b031b2a52903f4af5fa3a9
Rowan Victor <209355544+rowanvictor01@users.noreply.github.com>
2025-11-22 23:28:37 +0800

    Implement game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -12,6 +12,8 @@ public class GamePanel extends JPanel implements Runnable {
     // Time it takes to display each frame
     private final long TARGET_FRAME_TIME = 1_000_000_000L / 60;
 
+    private boolean running = false;
+
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
@@ -20,10 +22,44 @@ public class GamePanel extends JPanel implements Runnable {
 
     }
 
+    public void update() {
+        // code
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        // code
+    }
+
     // Game Loop
     @Override
     public void run() {
-        // code
+
+        long startTime = System.nanoTime();
+
+        while(running) {
+
+            long currentTime = System.nanoTime();
+            long elapsedTimeNs = currentTime - startTime;
+
+            if(elapsedTimeNs < TARGET_FRAME_TIME) {
+
+                long sleepTime = (TARGET_FRAME_TIME - elapsedTimeNs) / 1_000_000;
+
+                try {
+                    Thread.sleep(sleepTime);
+                } catch(InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+
+            }
+
+            update();
+            startTime = System.nanoTime();
+
+        }
+
     }
 
 }
